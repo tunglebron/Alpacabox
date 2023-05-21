cf30dd73eed88a7c5fca36db1d07c291da61c92f
Tung Le <55380075+tunglebron@users.noreply.github.com>
2023-05-21 18:20:48 +0700

    Update index.js

--- admin/src/index.js
+++ admin/src/index.js
@@ -7,7 +7,7 @@ import { ListContextProvider } from './context/listContext/ListContext';
 import { UserContextProvider } from './context/userContext/UserContext';
 
 //global.proxy = "//192.168.56.1:8000/api/"
-gloabal.proxy = "https://alpacabox.onrender.com/api/"
+global.proxy = "https://alpacabox.onrender.com/api/"
 
 ReactDOM.render(
   <React.StrictMode>
