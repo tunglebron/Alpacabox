3169a2e9d906352a98c931762dfbfadfb197648d
Tung Le <55380075+tunglebron@users.noreply.github.com>
2023-05-21 17:15:36 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthContextProvider } from './context/authContext/AuthContext'
 
-global.proxy = "//192.168.56.1:8000/api/"
+//global.proxy = "//192.168.56.1:8000/api/"
+global.proxy = "//localhost:8000/api/"
 
 ReactDOM.render(
   <React.StrictMode>
