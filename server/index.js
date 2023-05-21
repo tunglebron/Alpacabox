698d9dcb3e73dbdffa3f84ee831947846535451a
Tung Le <55380075+tunglebron@users.noreply.github.com>
2023-05-21 16:53:29 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.use("/api/users", userRoute)
 app.use("/api/movies", movieRoute)
 app.use("/api/lists", listRoute)
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
